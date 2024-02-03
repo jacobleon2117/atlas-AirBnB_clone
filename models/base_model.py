2a010ad87ade2d80ca337e52e0d505d7873ec6cd
Your Name <you@example.com>
2024-02-03 16:59:32 -0600

    adding __init__(self) for datetime

--- models/base_model.py
+++ models/base_model.py
@@ -7,3 +7,11 @@ class BaseModel:
     """
     Model for class Base
     """
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        name = type(self).__name__
+        return "[{}] ({}) {}".format(name, self.id, self.__dict__)
