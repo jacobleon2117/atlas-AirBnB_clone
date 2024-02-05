e83968100043f099d68be7556e9980f57184d3f4
Your Name <you@example.com>
2024-02-05 11:04:12 -0600

    adding code/ need to fix errors

--- models/base_model.py
+++ models/base_model.py
@@ -7,8 +7,19 @@ class BaseModel:
     """
     Model for class Base
     """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
+    def __init__(self, *args, **kwargs):
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "upated_at":
+                    setattr(self.key, datetime.strptime(value, time_format))
+                else:
+                    setattr(self, key, value)
+            else:
+                self.id = str(uuid.uuid4())
+
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,23 +6,25 @@ from models.base_model import BaseModel
 class TestBasemodel(unittest.TestCase):
     def test_init(self):
         my_model = BaseModel()
+
         self.assertIsNotNone(my_model.id)
         self.assertIsNotNone(my_model.created_at)
         self.assertIsNotNone(my_model.updated_at)
 
     def test_save(self):
         my_model = BaseModel()
+
         initial_updated_at = my_model.updated_at
         current_updated_at = my_model.save()
         self.assertNotEqual(initial_updated_at, current_updated_at)
 
     def test_to_dict(self):
         my_model = BaseModel()
+
         my_model_dict = my_model.to_dict()
         self.assertIsInstance(my_model_dict, dict)
-
-        self.assertEqual(my_model_dict["__class__"], "BaseModel")
-        self.assertEqual(my_model_dict["id"], my_model.id)
+        self.assertEqual(my_model_dict["__class__"], 'BaseModel')
+        self.assertEqual(my_model_dict['id'], my_model.id)
         self.assertEqual(my_model_dict["created_at"], my_model.created_at.isoformat())
         self.assertEqual(my_model_dict["updated_at"], my_model.created_at.isoformat())
 
