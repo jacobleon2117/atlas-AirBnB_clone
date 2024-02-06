ee01418c420ab810f432d9eb53ed677b0478530f
Your Name <you@example.com>
2024-02-06 10:28:03 -0600

    adding reload to FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -34,3 +34,22 @@ class FileStorage:
 
         for obj in all_objs.keys():
             obj_dict[obj] = all_objs[obj].to_dict()
+
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
+            json.dump(obj_dict, file)
+
+    def reload(self):
+        """
+        
+        """
+        if os.path.isFile(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
+                try:
+                    obj_dict = json.load(file)
+                    for key, value in obj_dict.items():
+                        class_name, obj_id = key.split(".")
+                        cls = eval(class_name)
+                        instance = cls(**values)
+                        FileStorage._object[key] = instance
+                except Exception:
+                    pass
