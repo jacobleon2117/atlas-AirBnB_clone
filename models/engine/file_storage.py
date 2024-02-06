c2e8e57418dfa6af39919d990b6a48ba40f138ab
Your Name <you@example.com>
2024-02-06 10:15:40 -0600

    adding to filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
 
 import json
+import os
 from os.path import isfile
 
 class FileStorage:
-    
+    """
+    obj takes
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def new(self, obj):
+        """
+        Takes passed args then stores 
+        """
+        obj_cls_name = obj.__class__.__name__
+        key = "{}.{}".format(obj_cls_name, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def all(self):
+        
