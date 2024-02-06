22174e0c3cd804a88fb6d84ccc23e74ba7cb12a8
Your Name <you@example.com>
2024-02-06 11:57:17 -0600

    Adding code to rest of files and console

--- console.py
+++ console.py
@@ -1,7 +1,5 @@
-#!/usr/bin/python
-"""
-Module for console
-"""
+#!/usr/bin/python3
+
 import cmd
 import re
 import shlex
@@ -14,42 +12,6 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.city import City
-
-
-def split_curly_braces(e_arg):
-    """
-    Splits the curly braces for the update method
-    """
-    curly_braces = re.search(r"\{(.*?)\}", e_arg)
-
-    if curly_braces:
-        id_with_comma = shlex.split(e_arg[:curly_braces.span()[0]])
-        id = [i.strip(",") for i in id_with_comma][0]
-
-        str_data = curly_braces.group(1)
-        try:
-            arg_dict = ast.literal_eval("{" + str_data + "}")
-        except Exception:
-            print("**  invalid dictionary format **")
-            return
-        return id, arg_dict
-    else:
-        commands = e_arg.split(",")
-        if commands:
-            try:
-                id = commands[0]
-            except Exception:
-                return "", ""
-            try:
-                attr_name = commands[1]
-            except Exception:
-                return id, ""
-            try:
-                attr_value = commands[2]
-            except Exception:
-                return id, attr_name
-            return f"{id}", f"{attr_name} {attr_value}"
-
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -69,12 +31,32 @@ class HBNBCommand(cmd.Cmd):
         """
         EOF (Ctrl+D) signal to exit the program.
         """
+        print("Exiting HBNB console")
         return True
 
     def do_quit(self, arg):
         """
         Quit command to exit the program.
         """
+        print("Exiting HBNB console")
+        return True
+
+    def validate_class(self, class_name):
+        """
+        Validate if the class name is valid.
+        """
+        if class_name not in self.valid_classes:
+            print("** Invalid class name. Valid classes are: {} **".format(", ".join(self.valid_classes)))
+            return False
+        return True
+
+    def validate_instance(self, class_name, instance_id):
+        """
+        Validate if the instance exists.
+        """
+        if class_name not in storage.all():
+            print("** No instance found for class '{}' and id '{}' **".format(class_name, instance_id))
+            return False
         return True
 
     def do_create(self, arg):
@@ -82,38 +64,38 @@ class HBNBCommand(cmd.Cmd):
         Create a new instance of BaseModel and save it to the JSON file.
         Usage: create <class_name>
         """
-        commands = shlex.split(arg)
-
-        if len(commands) == 0:
+        class_name = arg.strip()
+        if not class_name:
             print("** class name missing **")
-        elif commands[0] not in self.valid_classes:
-            print("** class doesn't exist **")
-        else:
-            new_instance = eval(f"{commands[0]}()")
-            storage.save()
+            return
+
+        if not self.validate_class(class_name):
+            return
+
+        try:
+            new_instance = eval(f"{class_name}()")
+            new_instance.save()
             print(new_instance.id)
+        except Exception as e:
+            print("** Error creating instance: {} **".format(e))
 
     def do_show(self, arg):
-        """k
+        """
         Show the string representation of an instance.
         Usage: show <class_name> <id>
         """
         commands = shlex.split(arg)
+        if len(commands) != 2:
+            print("** Usage: show <class_name> <id> **")
+            return
 
-        if len(commands) == 0:
-            print("** class name missing **")
-        elif commands[0] not in self.valid_classes:
-            print("** class doesn't exist **")
-        elif len(commands) < 2:
-            print("** instance id missing **")
-        else:
-            objects = storage.all()
+        class_name, instance_id = commands
+        if not self.validate_class(class_name):
+            return
+        if not self.validate_instance(class_name, instance_id):
+            return
 
-            key = "{}.{}".format(commands[0], commands[1])
-            if key in objects:
-                print(objects[key])
-            else:
-                print("** no instance found **")
+        print(storage.all()[class_name + '.' + instance_id])
 
     def do_destroy(self, arg):
         """
@@ -121,65 +103,45 @@ class HBNBCommand(cmd.Cmd):
         Usage: destroy <class_name> <id>
         """
         commands = shlex.split(arg)
+        if len(commands) != 2:
+            print("** Usage: destroy <class_name> <id> **")
+            return
 
-        if len(commands) == 0:
-            print("** class name missing **")
-        elif commands[0] not in self.valid_classes:
-            print("** class doesn't exist **")
-        elif len(commands) < 2:
-            print("** instance id missing **")
-        else:
-            objects = storage.all()
-            key = "{}.{}".format(commands[0], commands[1])
-            if key in objects:
-                del objects[key]
-                storage.save()
-            else:
-                print("** no instance found **")
+        class_name, instance_id = commands
+        if not self.validate_class(class_name):
+            return
+        if not self.validate_instance(class_name, instance_id):
+            return
+
+        key = class_name + '.' + instance_id
+        del storage.all()[key]
+        storage.save()
+
     def do_all(self, arg):
         """
         Print the string representation of all instances or a specific class.
-        Usage: <User>.all()
-                <User>.show()
+        Usage: all [class_name]
         """
         objects = storage.all()
-
-        commands = shlex.split(arg)
-
-        if len(commands) == 0:
-            for key, value in objects.items():
-                print(str(value))
-        elif commands[0] not in self.valid_classes:
-            print("** class doesn't exist **")
-        else:
-            for key, value in objects.items():
-                if key.split('.')[0] == commands[0]:
-                    print(str(value))
+        if arg:
+            class_name = arg.strip()
+            if not self.validate_class(class_name):
+                return
+            objects = {k: v for k, v in objects.items() if k.startswith(class_name + ".")}
+        for obj in objects.values():
+            print(obj)
 
     def do_count(self, arg):
         """
-        Counts and retrieves the number of instances of a class
-        usage: <class name>.count()
+        Counts and retrieves the number of instances of a class.
+        Usage: <class name>.count()
         """
-        objects = storage.all()
+        class_name = arg.strip()
+        if not self.validate_class(class_name):
+            return
 
-        commands = shlex.split(arg)
-
-        if arg:
-            cls_nm = commands[0]
-
-        count = 0
-
-        if commands:
-            if cls_nm in self.valid_classes:
-                for obj in objects.values():
-                    if obj.__class__.__name__ == cls_nm:
-                        count += 1
-                print(count)
-            else:
-                print("** invalid class name **")
-        else:
-            print("** class name missing **")
+        count = sum(1 for obj in storage.all().values() if obj.__class__.__name__ == class_name)
+        print(count)
 
     def do_update(self, arg):
         """
@@ -187,104 +149,28 @@ class HBNBCommand(cmd.Cmd):
         Usage: update <class_name> <id> <attribute_name> "<attribute_value>"
         """
         commands = shlex.split(arg)
+        if len(commands) < 4:
+            print("** Usage: update <class_name> <id> <attribute_name> <attribute_value> **")
+            return
 
-        if len(commands) == 0:
-            print("** class name missing **")
-        elif commands[0] not in self.valid_classes:
-            print("** class doesn't exist **")
-        elif len(commands) < 2:
-            print("** instance id missing **")
-        else:
-            objects = storage.all()
+        class_name, instance_id, attr_name = commands[:3]
+        if not self.validate_class(class_name):
+            return
+        if not self.validate_instance(class_name, instance_id):
+            return
 
-            key = "{}.{}".format(commands[0], commands[1])
-            if key not in objects:
-                print("** no instance found **")
-            elif len(commands) < 3:
-                print("** attribute name missing **")
-            elif len(commands) < 4:
-                print("** value missing **")
-            else:
-                obj = objects[key]
-                curly_braces = re.search(r"\{(.*?)\}", arg)
+        attr_value = ' '.join(commands[3:])
 
-                if curly_braces:
-                    try:
-                        str_data = curly_braces.group(1)
-
-                        arg_dict = ast.literal_eval("{" + str_data + "}")
-
-                        attribute_names = list(arg_dict.keys())
-                        attribute_values = list(arg_dict.values())
-                        try:
-                            attr_name1 = attribute_names[0]
-                            attr_value1 = attribute_values[0]
-                            setattr(obj, attr_name1, attr_value1)
-                        except Exception:
-                            pass
-                        try:
-                            attr_name2 = attribute_names[1]
-                            attr_value2 = attribute_values[1]
-                            setattr(obj, attr_name2, attr_value2)
-                        except Exception:
-                            pass
-                    except Exception:
-                        pass
-                else:
-
-                    attr_name = commands[2]
-                    attr_value = commands[3]
-
-                    try:
-                        attr_value = eval(attr_value)
-                    except Exception:
-                        pass
-                    setattr(obj, attr_name, attr_value)
-
-                obj.save()
+        obj = storage.all()[class_name + '.' + instance_id]
+        setattr(obj, attr_name, attr_value)
+        storage.save()
 
     def default(self, arg):
         """
-        Default behavior for cmd module when input is invalid
+        Default behavior for cmd module when input is invalid.
         """
-        arg_list = arg.split('.')
-
-        cls_nm = arg_list[0]  # incoming class name
-
-        command = arg_list[1].split('(')
-
-        cmd_met = command[0]  # incoming command method
-
-        e_arg = command[1].split(')')[0]  # extra arguments
-
-        method_dict = {
-                'all': self.do_all,
-                'show': self.do_show,
-                'destroy': self.do_destroy,
-                'update': self.do_update,
-                'count': self.do_count
-                }
-
-        if cmd_met in method_dict.keys():
-            if cmd_met != "update":
-                return method_dict[cmd_met]("{} {}".format(cls_nm, e_arg))
-            else:
-                if not cls_nm:
-                    print("** class name missing **")
-                    return
-                try:
-                    obj_id, arg_dict = split_curly_braces(e_arg)
-                except Exception:
-                    pass
-                try:
-                    call = method_dict[cmd_met]
-                    return call("{} {} {}".format(cls_nm, obj_id, arg_dict))
-                except Exception:
-                    pass
-        else:
-            print("*** Unknown syntax: {}".format(arg))
-            return False
-
+        print("** Unknown command '{}' **".format(arg))
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
+
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,35 +1,53 @@
 #!/usr/bin/python3
-
+"""
+Module for serializing and deserializing data
+"""
 import json
 import os
-from os.path import isfile
+from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.city import City
+
 
 class FileStorage:
     """
-    obj takes
+    FileStorage class for storing, serializing and deserializing data
     """
     __file_path = "file.json"
+
     __objects = {}
 
     def new(self, obj):
         """
-        Takes passed args then stores 
+         Sets an object in the __objects dictionary with a key of 
+         <obj class name>.id.
         """
         obj_cls_name = obj.__class__.__name__
+
         key = "{}.{}".format(obj_cls_name, obj.id)
+
         FileStorage.__objects[key] = obj
+
 
     def all(self):
         """
-        returns filestorage
+        Returns the __objects dictionary. 
+        It provides access to all the stored objects.
         """
-        return FileStorage.__objects
-    
+        return  FileStorage.__objects
+
+
     def save(self):
         """
-        Save to json format for storing 
+        Serializes the __objects dictionary into 
+        JSON format and saves it to the file specified by __file_path.
         """
         all_objs = FileStorage.__objects
+
         obj_dict = {}
 
         for obj in all_objs.keys():
@@ -40,16 +58,20 @@ class FileStorage:
 
     def reload(self):
         """
-        
+        This method deserializes the JSON file
         """
         if os.path.isfile(FileStorage.__file_path):
             with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
                 try:
                     obj_dict = json.load(file)
-                    for key, values in obj_dict.items():
-                        class_name, obj_id = key.split(".")
+
+                    for key, value in obj_dict.items():
+                        class_name, obj_id = key.split('.')
+
                         cls = eval(class_name)
-                        instance = cls(**values)
-                        FileStorage._object[key] = instance
+
+                        instance = cls(**value)
+
+                        FileStorage.__objects[key] = instance
                 except Exception:
                     pass
