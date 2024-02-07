de2d0c6936326ed4bbf7388eb502ba3a55b6f5fe
jacobleon2117 <143765559+jacobleon2117@users.noreply.github.com>
2024-02-07 11:41:29 -0600

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
-        
+
         for key, value in kwargs.items():
             if key == "__class__":
                 continue
